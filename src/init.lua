9a800beddd829a3b3bab74a315fbd28d419c8463
zblox164 <68709543+zblox164@users.noreply.github.com>
2024-12-18 12:48:42 +0000

    Update init.lua

--- src/init.lua
+++ src/init.lua
@@ -1,12 +1,3 @@
---[[
-
-     Scrypt Framework [2024 - 2025] 
-     
-     DO NOT use without permission from the owner.
-     Written by: zblox164
-     
---]]
-
 local RunService: RunService = game:GetService("RunService")
 
 if RunService:IsServer() then
