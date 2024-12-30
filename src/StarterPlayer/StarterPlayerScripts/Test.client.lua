6c7fa6dcbb1d85c4f6250c0443655b8c03967a39
zblox164 <zblox164@gmail.com>
2024-12-30 03:51:17 -0700

    Removed test files

--- src/StarterPlayer/StarterPlayerScripts/Test.client.lua
+++
@@ -1,6 +0,0 @@
-local Scrypt = require(game:GetService("ReplicatedStorage"):WaitForChild("Scrypt"))
-Scrypt.Init():Wait()
-
-Scrypt.ClientNetwork.ListenForPacket("TEST", true, function(...)  
-    print("Received packet", ...)
-end) 
